6cca8b2d73b94779837fdbe233bc0a5143598a02
flybizit-dev <72803639+flybizit-dev@users.noreply.github.com>
2021-01-01 14:09:22 +0530

    First Commit
    Music Bot for Discord , in Js

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'your Bot Token',
+        prefix: '-',
+        activity: 'Pleading you with the Music, Gift from IndiaVale, discord.gg/indiavale',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
